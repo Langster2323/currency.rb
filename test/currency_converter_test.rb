1aec9e6919cf3cd4f64ee16596a9266d356a5d30
Ahkeem Lang <langahkeem@gmail.com>
2016-07-05 08:38:32 -0500

    No major changes but, I'm understanding the syntax and model a lot more than before.

--- test/currency_converter_test.rb
+++ test/currency_converter_test.rb
@@ -4,10 +4,10 @@ require_relative '../lib/currency'
 
 class CurrencyConverter < Minitest::Test
   def test_currency_converter_exist
-    assert CurrencyConverter.new(3)
+    assert Converter.new(3)
   end
 
   def test_currency_addition_converter
-    assert_equal (CurrencyConverter.new(7.7), CurrencyConverter.new(5) * CurrencyConverter) + 2
+      assert_equal(Converter.new(7.7), 5 * CurrencyConverter ) + 2
   end
 end
