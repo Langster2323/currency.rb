bac0c6a8f7f668ddf5f072b633774e87fb6c28d8
Ahkeem Lang <langahkeem@gmail.com>
2016-07-08 13:21:37 -0500

    converter finally works thanks to some great help

--- test/currency_converter_test.rb
+++ test/currency_converter_test.rb
@@ -4,10 +4,10 @@ require_relative '../lib/currency'
 
 class CurrencyConverter < Minitest::Test
   def test_currency_converter_exist
-    assert Converter.new(3)
+    assert Converter.new.convert(Currency.new(5, "USD"), "EUR")
   end
 
   def test_currency_addition_converter
-      assert_equal(Converter.new(7.7), 5 * CurrencyConverter ) + 2
+
   end
 end
